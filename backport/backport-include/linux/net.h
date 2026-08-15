559197405ec13cd8385bbac0bf7a2b60aecc2a65
Felix Fietkau <nbd@nbd.name>
2026-08-15 20:04:22 +0000

    backport-include: backport struct sockaddr_unsized
    The in-kernel socket API takes struct sockaddr_unsized since 6.19.
    Add the struct and wrap kernel_connect() so that it casts back to
    struct sockaddr on older kernels. Used by the ath10k/ath11k/ath12k
    QMI code.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/net.h
+++ backport/backport-include/linux/net.h
@@ -1,7 +1,9 @@
 #ifndef __BACKPORT_LINUX_NET_H
 #define __BACKPORT_LINUX_NET_H
+#include <linux/version.h>
 #include_next <linux/net.h>
 #include <linux/static_key.h>
+#include <linux/socket.h>
 
 
 #ifndef SOCKWQ_ASYNC_NOSPACE
@@ -10,5 +12,19 @@
 #ifndef SOCKWQ_ASYNC_WAITDATA
 #define SOCKWQ_ASYNC_WAITDATA   SOCK_ASYNC_WAITDATA
 #endif
+
+#if LINUX_VERSION_IS_LESS(6,19,0)
+/*
+ * The in-kernel socket API takes struct sockaddr_unsized since 6.19.
+ * Older kernels still expect struct sockaddr here.
+ */
+static inline int backport_kernel_connect(struct socket *sock,
+					  struct sockaddr_unsized *addr,
+					  int addrlen, int flags)
+{
+	return kernel_connect(sock, (struct sockaddr *)addr, addrlen, flags);
+}
+#define kernel_connect backport_kernel_connect
+#endif /* < 6.19 */
 
 #endif /* __BACKPORT_LINUX_NET_H */
--- backport/backport-include/linux/socket.h
+++ backport/backport-include/linux/socket.h
@@ -1,5 +1,6 @@
 #ifndef __BACKPORT_SOCKET_H
 #define __BACKPORT_SOCKET_H
+#include <linux/version.h>
 #include_next <linux/socket.h>
 
 #ifndef SOL_NFC
@@ -9,6 +10,25 @@
  */
 #define SOL_NFC		280
 #endif
+
+#if LINUX_VERSION_IS_LESS(6,19,0)
+/**
+ * struct sockaddr_unsized - Unspecified size sockaddr for callbacks
+ * @sa_family: Address family (AF_UNIX, AF_INET, AF_INET6, etc.)
+ * @sa_data: Flexible array for address data
+ *
+ * This structure is designed for callback interfaces where the
+ * total size is known via the sockaddr_len parameter. Unlike struct
+ * sockaddr which has a fixed 14-byte sa_data limit or struct
+ * sockaddr_storage which has a fixed 128-byte sa_data limit, this
+ * structure can accommodate addresses of any size, but must be used
+ * carefully.
+ */
+struct sockaddr_unsized {
+	__kernel_sa_family_t	sa_family;	/* address family, AF_xxx */
+	char			sa_data[];	/* flexible address data */
+};
+#endif /* < 6.19 */
 
 #ifndef __sockaddr_check_size
 #define __sockaddr_check_size(size)	\
